ac5f0f2f9e7dd2d3ba59d756e6b65a4e902f589b
tianya <tianya@echatsoft.com>
2021-01-26 20:42:02 +0800

    The first commit

---
+++ svgIconBuilder.js
@@ -0,0 +1,58 @@
+const fs = require('fs'),
+    svg2ttf = require('svg2ttf'),
+    ttf2woff = require('ttf2woff2'),
+    ttf2woff2 = require('ttf2woff2'),
+    ttf2eot = require('ttf2eot'),
+    collectionBuilder = require('./svgfile2IconCollection'),
+    supportTypes = ['ttf', /* 'eot',  'woff', 'woff2'*/],
+    convertors = { ttf: (buf) => new global.Buffer(buf), eot: ttf2eot, woff: ttf2woff, woff2: ttf2woff2 },
+    projects = [
+        {
+            name: 'echat',
+            svgsPath: './svgs',
+            dest: './fonts/echat'
+        }
+    ];
+
+let i = 0;
+for (; i < projects.length; i++) {
+    let pcfg = projects[i];
+    collectionBuilder(pcfg).then(() => {
+        buildFontFile(pcfg);
+    }, (reason) => {
+        console.log(reason);
+    });
+}
+
+/**
+ * @param {promise} promise
+ 
+function collBuilderLooper(promise,index) {
+    promise.then(() => {
+        console.log('succ');
+        buildFontFile(projects[currentProjIndex]);
+        currentProjIndex++;
+        collBuilderLooper(collectionBuilder(projects[currentProjIndex]));
+    }, (reason) => {
+        console.error(reason);
+        currentProjIndex++;
+        collBuilderLooper(collectionBuilder(projects[currentProjIndex]));
+    });
+}
+*/
+
+function buildFontFile(fontCfg) {
+
+    var ttf = svg2ttf(fs.readFileSync(GetABSpath(`./fonts/iconCollection-${fontCfg.name}.svg`), 'utf8'), {});
+
+    for (let i = 0; i < supportTypes.length; i++) {
+        if (convertors[supportTypes[i]]) {
+            fs.writeFileSync(GetABSpath(`${fontCfg.dest}.${supportTypes[i]}`), convertors[supportTypes[i]](ttf.buffer));
+        }
+    }
+}
+
+
+function GetABSpath(path) {
+    return [__dirname, path].join('/');
+}
---
+++ svgfile2IconCollection.js
@@ -0,0 +1,139 @@
+/**
+ * 此功能是将整个项目的res/fonts/svgs中的svg文件集合成一个fontface，提供给svgTottf来为字体文件
+ */
+const fs = require('fs'),
+    OS = require('os'),
+    CP = require('child_process'),
+    SVGIcons2SVGFontStream = require('svgicons2svgfont');
+/**
+ * @param {Object} project 配置参数
+ * @param {String} project.name 字体图标的，字体名
+ * @param {String} project.svgsPath svg字体图标所在目录
+ */
+module.exports = function svgCollectionBuilder(project) {
+
+    const defer = Defer(),
+        icons = [],
+        fontStream = new SVGIcons2SVGFontStream({
+            fontName: project.name,
+            normalize: true,
+            fontHeight: 1024,
+        });
+
+    let streamRes = false;
+    // Setting the font destination
+    fontStream.pipe(fs.createWriteStream(GetABSpath(`./fonts/iconCollection-${project.name}.svg`)))
+        .on('finish', function () {
+            console.log(`${project.name} Font successfully created!`);
+            streamRes = true;
+        })
+        .on('error', function (err) {
+            console.log(err);
+
+        }).on('close', () => {
+            setTimeout(() => {
+                if (streamRes) {
+                    defer.resolve();
+                } else {
+                    defer.reject();
+                }
+                openIconPageToChrome();
+            }, 300);
+        });
+
+    const svgFiles = fs.readdirSync(GetABSpath(project.svgsPath), { encoding: 'utf8' });
+
+    if (svgFiles.length) {
+
+        let glyphMetadata;
+        for (let i = 0, len = svgFiles.length; i < len; i++) {
+            // Writing glyphs
+            glyphMetadata = GetSvgNameCode(svgFiles[i]);
+            if (!glyphMetadata) {
+                continue;
+            }
+            var { nameString: name, codeString: code } = glyphMetadata;
+            icons.push({ code, name });
+            const glyph = fs.createReadStream(GetABSpath(`${project.svgsPath}/${svgFiles[i]}`));
+            glyph.metadata = glyphMetadata;
+            fontStream.write(glyph);
+
+        }
+        fs.writeFile('./fonts/icons.json', JSON.stringify(icons), () => { });
+
+    } else {
+        setTimeout(() => {
+            defer.reject(`No svg file in directory :=> ${project.svgsPath}`);
+        }, 400);
+    }
+
+    // Do not forget to end the stream
+    fontStream.end(() => {
+        console.log(...arguments);
+    });
+    return defer.promise;
+};
+
+function GetSvgNameCode(fileName) {
+
+    let names = fileName.split('.');
+
+    if (names[names.length - 1] !== 'svg') {
+        return null;
+    } else {
+        names = names[0];
+    }
+
+    names = names.split('$$');
+
+    if (names.length) {
+        return {
+            unicode: [String.fromCharCode(`0x${names[0]}`)],
+            name: names.reverse().join(''),
+            codeString: names[1],
+            nameString: names[0],
+        };
+    }
+    return null;
+}
+
+function GetABSpath(path) {
+    return [__dirname, path].join('/');
+}
+
+
+/**创建一个defer*/
+function Defer() {
+    function PromiseMaker() {
+        let resolver,
+            rejecter,
+            promise = new Promise(function (resolve, reject) {
+                resolver = resolve;
+                rejecter = reject;
+            });
+
+        this.promise = promise;
+        this.resolve = (result) => {
+            resolver(result);
+        };
+        this.reject = (reason) => {
+            rejecter(reason);
+        };
+
+        /**catch warn */
+        promise.then(() => { }, () => { });
+        return this;
+    }
+    const PM = new PromiseMaker();
+    return PM;
+}
+
+function openIconPageToChrome() {
+    var currentPath = __dirname;
+    if (OS.platform == 'darwin') {
+        CP.exec(`open -a "Google Chrome" file://${currentPath}/index.html`);
+    } else if (OS.platform == 'win32') {
+        CP.exec(`start chrome file://${currentPath}/index.html`)
+    }
+
+}
