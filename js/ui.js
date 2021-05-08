7221b7acc3b8f3cc232e45112664e0bb3c51cd11
zhoutianya <tianya@echatsoft.com>
2021-05-08 18:33:51 +0800

    增加批量上传图标功能

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,10 @@
-window.$ = function (selector) { return document.querySelectorAll(selector) }
+
 
 document.addEventListener('DOMContentLoaded', function (e) {
+    var $$ = function (selector) { return document.querySelectorAll(selector) }
     //滚动时固定头部
-    var headerNav = $('.project-opts')[0];
-    var icons = $('#icons')[0];
+    var headerNav = $$('.nav-header')[0];
+    var icons = $$('#icons')[0];
 
     window.addEventListener('scroll', function (e) {
         if (window.scrollY > 0) {
@@ -15,14 +16,14 @@ document.addEventListener('DOMContentLoaded', function (e) {
         }
     });
     //图标颜色
-    var st = $('#cc-style')[0];
-    var scinput = $('#icon_color')[0];
+    var st = $$('#cc-style')[0];
+    var scinput = $$('#icon_color')[0];
     scinput.addEventListener('input', function (e) {
         setIconStyle({ color: this.value });
     });
 
     //图标大小
-    var szinput = $('#icon_size')[0];
+    var szinput = $$('#icon_size')[0];
     szinput.addEventListener('change', function (e) {
         setIconStyle({ size: this.value });
     });
@@ -38,5 +39,74 @@ document.addEventListener('DOMContentLoaded', function (e) {
         scinput.value = color;
         szinput.value = size;
     }
+
+    var upload = $$('#uploadinput')[0],
+        exts = $$('#ext-icons')[0],
+        willUploads = [];
+
+    upload.addEventListener('change', function (evt) {
+
+        if (willUploads.length == 8) {
+            return alert('一次上传最多8个');
+        }
+
+        for (let i = 0; i < this.files.length; i++) {
+            Read(this.files[i])
+        }
+
+        function Read(file) {
+            var FR = new FileReader();
+            FR.onload = evt => {
+                try {
+                    var svgDom = new DOMParser().parseFromString(evt.target.result, 'application/xml');
+                    if (svgDom.firstElementChild.tagName.toUpperCase() !== 'SVG') {
+                        throw 'ç'
+                    }
+                } catch (e) {
+                    return alert("不是标准的svg文件");
+                }
+
+                if (willUploads.length < 8) {
+                    exts.innerHTML += "<div class='upload-svg'>" + evt.target.result + "</div>";
+                    willUploads.push({ name: file.name, text: evt.target.result });
+                }
+            }
+            FR.readAsText(file, 'utf-8');
+        }
+    });
+    $$('#upload-cancel')[0].addEventListener('click', function () {
+        willUploads = [];
+        exts.innerHTML = '';
+    });
+    $$('#upload-btn')[0].addEventListener('click', function () {
+        var fileMap = willUploads.reduce(function (prevs, item) {
+            prevs[item.name] = item.text;
+            return prevs;
+        }, {});
+
+        $.ajax({
+            url: '/svg/upload',
+            method: 'post',
+            data: fileMap
+        })
+    });
 });
 
+
+function renewFontIcon() {
+    var tm = Date.now();
+    var fontTpl = `@font-face {
+        font-family: echat;
+        src: url('./fonts/echat.eot?t=${tm}');
+        /* IE9*/
+        src: url('./fonts/echat.eot?t=${tm}#iefix') format('embedded-opentype'),
+            /* IE6-IE8 */
+            url('./fonts/echat.woff?t=${tm}') format('woff'),
+            /* chrome, firefox */
+            url('./fonts/echat.ttf?t=${tm}') format('truetype'),
+            /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
+            url('./fonts/echat.svg?t=${tm}#echat') format('svg');
+        /* iOS 4.1- */
+    }`
+    $('#font-style')[0].innerHTML = fontTpl;
+}
--- server/index.js
+++ server/index.js
@@ -1,45 +1,66 @@
-var http = require('http');
+
+var Express = require('express');
+var { urlencoded } = require('body-parser');
+var http = Express();
 var fs = require('fs');
-var URL = require('url');
+const builder = require('../svgIconBuilder');
 
+const SERVER_PORT = "8089";
 
-http.createServer(function (req, resp) {
-    // 解析请求，包括文件名
-    var fullUrl = `http://${req.headers.host}${req.url}`,
-        reqInfo = URL.parse(fullUrl),
-        params = new URL.URLSearchParams(reqInfo.search);
+http.use(urlencoded({
+    extended: true
+}));
 
-    if (req.method === 'OPTIONS') {
-        resp.setHeader('Access-Control-Allow-Headers', '_csrf,Content-Type');
+http.all("/svg/get", function (req, resp, params) {
+    var fn = req.query['fn'];
+    if (!fn) {
+        return respone(resp, '');
     }
-    resp.setHeader('Access-Control-Allow-Origin', '*');
+    svgFile = fs.readFileSync(GetSvgFolder(fn), { encoding: 'utf-8' });
 
-    requests[reqInfo.pathname](req, resp, params);
-    resp.write
-}).listen("8089");
+    respone(resp, svgFile);
 
-const requests = {
-    /**
-     * @param {IncomingMessage} req 
-     * @param {ServerResponse} resp 
-     * @param {URLSearchParams} params 
-     */
-    "/getsvg": function (req, resp, params) {
-        var fn = params.get('fn'),
-            svgFile = fs.readFileSync(GetSvgFolder(fn), { encoding: 'utf-8' });
+});
 
-        resp.writeHead(200).end(svgFile);
+http.post("/svg/save", function (req, resp, params) {
+    var body = req.body,
+        fn = body['fn'],
+        fileString = body['svgFile'];
 
-    },
-    "/saveSvg": function (req, resp, params) {
-        var fn = params.get('fn'),
-            fileString = params.get('svgFile');
+    resp.setHeader("content-type", "application/json; charset=UTF-8")
 
-        fs.writeFileSync(GetSvgFolder(fn), fileString, { encoding: 'utf-8' });
-
-        resp.writeHead(200).end(`{succ:true}`);
+    if (!fn || !fileString) {
+        return respone(resp, respRes(false));
     }
+
+    fs.writeFileSync(GetSvgFolder(fn), fileString, { encoding: 'utf-8' });
+
+    builder({
+        svgFolder: `${__dirname}/../svgs`,
+        outputFolder: `${__dirname}/../fonts`,
+        fontTypes: ['ttf', 'eot', 'woff', 'woff2']
+    }).then((res) => {
+        respone(resp, respRes(true));
+    }, (err) => {
+        respone(resp, respRes(false));
+    })
+
+});
+
+http.post("/svg/upload", function (req, resp) {
+    debugger
+})
+
+function respRes(bool) {
+    return JSON.stringify({ succ: !!bool });
 }
+function respone(resp, res, code = 200,) {
+    resp.writeHead(code).end(res);
+}
+
+http.listen(SERVER_PORT, () => {
+    console.log(`Server listening at http://localhost:${SERVER_PORT}`)
+});
 
 function GetSvgFolder(fileName) {
     return `${__dirname}/../svgs/${fileName}`;
